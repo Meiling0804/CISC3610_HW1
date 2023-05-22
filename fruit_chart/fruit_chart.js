34f2e9d5118496d2ac4b46dd81acdc5c3765201b
Meiling0804 <70685065+Meiling0804@users.noreply.github.com>
2023-05-22 01:41:27 -0400

    Add files via upload

---
+++ fruit_chart/fruit_chart.js
@@ -0,0 +1,10 @@
+window.onload = function () {
+var fruits = [
+	{name: "Apple", quantity: 20, color: "red"},
+	{name: "Orange", quantity: 10, color: "orange"},
+	{name: "Banana", quantity: 15, color: "yellow"},
+	{name: "Kiwi", quantity: 5, color: "green"},
+	{name: "Blueberry", quantity: 5, color: "blue"},
+	{name: "Grapes", quantity: 10, color: "purple"}
+];
+}
